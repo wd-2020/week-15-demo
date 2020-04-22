fa1563de1dd83ff6db9b81f9f385fc4e5fadcdfa
Karl Stolley <karl.stolley@gmail.com>
2020-04-21 20:00:28 -0500

    Add function constructor that walks back and forth through an array

--- site.js
+++ site.js
@@ -1,1 +1,29 @@
 document.querySelector('html').className = "js";
+
+function FancyArray(arr) {
+  this.arr = arr;
+  this.count = this.arr.length;
+  this.current = 0;
+
+  this.countEm = function() {
+    return this.count;
+  }
+
+  this.nextItem = function() {
+    this.current++;
+    if (this.current >= this.count) {
+      this.current = 0;
+    }
+    return this.arr[this.current];
+  }
+  this.prevItem = function() {
+    this.current--;
+    if (this.current < 0) {
+      this.current = this.count - 1;
+    }
+    return this.arr[this.current];
+  }
+  this.currItem = function() {
+    return this.arr[this.current];
+  }
+}

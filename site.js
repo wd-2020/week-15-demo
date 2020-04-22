f241962b29be5c0cac5103b04eb183c7c08220bf
Karl Stolley <karl.stolley@gmail.com>
2020-04-21 21:28:03 -0500

    Create and wire up events for the controls

--- site.js
+++ site.js
@@ -1,9 +1,52 @@
 document.querySelector('html').className = "js";
 
+/*
+  Set up, create, and append the HTML for controlling the slideshow...
+*/
+var buttons = [
+  {
+    id: 'prev',
+    href: '#prev',
+    text: 'Previous'
+  },
+  {
+    id: 'play',
+    href: '#play',
+    text: 'Play'
+  },
+  {
+    id: 'next',
+    href: '#next',
+    text: 'Next'
+  },
+];
+var controls = document.createElement('nav');
+controls.className = 'controls';
+var controls_ul = document.createElement('ul');
+var controls_li;
+var controls_a;
+for (var b of buttons) {
+  controls_li = document.createElement('li');
+  controls_a = document.createElement('a');
+  controls_a.href = b.href;
+  controls_a.id = b.id;
+  controls_a.innerText = b.text;
+  controls_li.appendChild(controls_a);
+  controls_ul.appendChild(controls_li);
+}
+controls.appendChild(controls_ul);
+var header = document.querySelector('header');
+header.appendChild(controls);
+
+/*
+  Create the slideshow object...
+*/
+
 var slideshow = new SlideShow('.slides li');
+// Uncomment this line to have the slideshow play automatically:
 // slideshow.play();
 
-var controls = document.querySelector('.controls');
+// This references the controls object created above
 controls.addEventListener('click', function(event) {
   if (event.target.id === 'prev') {
     event.preventDefault();
